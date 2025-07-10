f9c641c1798cbdee25240bcd8d46e109ebfab5c5
zdnemz <zidanemz69@gmail.com>
2025-07-10 20:13:31 +0800

    initialize project

---
+++ src/main/java/manajemenItem/ManajemenItem.java
@@ -0,0 +1,13 @@
+package manajemenItem;
+
+/**
+ *
+ * @author ZidaneMZ
+ */
+
+public class ManajemenItem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
